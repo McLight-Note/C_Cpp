c48db13c3fe9d52345b9b9b652c107546617d8c1
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-08-17 16:12:56 +0900

    +

--- Lesson-6/main.c
+++ Lesson-6/main.c
@@ -9,17 +9,23 @@ int main(){
     char item[50] = "";
     float price = 0.0f;
     int quantity = 0;
-    char currency = "$";
+    char currency = '$';
     float total = 0.0f;
 
     printf("What item would you like to buy?: ");
     fgets(item, sizeof(item), stdin);
+    item[strlen(item) - 1] = '\0';
 
     printf("What is the price for the each item?: ");
-    scanf("%f\n", &price);
+    scanf("%f", &price);
 
     printf("How many would you like to buy?: ");
-    scanf("%d\n", &quantity);
+    scanf("%d", &quantity);
+
+    total = price * quantity;
+
+    printf("\nYou have bought %d %s/s\n", quantity, item);
+    printf("%c%.2f\n", currency, total);
 
     return 0;
 }
