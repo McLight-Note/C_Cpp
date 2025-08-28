548d2b09fec67b0bcdf63078fac7986ca4019b50
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-08-28 17:46:29 +0900

    +

--- Lesson-8/main.c
+++ Lesson-8/main.c
@@ -2,11 +2,20 @@
 #include <math.h>
 
 int main(){
+
     int x = 9;
 
     // x = sqrt(x);
-    x = pow(x, 2);
-
+    // x = pow(x, 2);
+    // x = ceil(x);
+    // x = floor(x);
+    // x = round(x);
+    // x = abs(x);
+    // x = log(x);
+    // x = sin(x);
+    // x = cos(x);
+    // x = tan(x);
+    
     printf("%d", x);
 
     return 0;
