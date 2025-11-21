739575d61d005e74b8ae821cd3f4d0ea9d684e4b
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-11-21 11:01:06 +0900

    +

---
+++ C#/Lesson-7/Lesson-8/Lesson-8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = int.Parse(Console.ReadLine());
+
+        bool result = (a == 20 || b == 20 || (a + b == 20));
+
+        Console.WriteLine("Result: " + result);
+    }
+}
